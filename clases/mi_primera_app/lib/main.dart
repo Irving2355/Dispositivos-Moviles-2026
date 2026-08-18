5de7a784a5bf85c26ec1a54fd1e2b45c0453f491
Irving2355 <it235597@gmail.com>
2026-08-17 19:44:15 -0600

    Clase 2026-08-17 - Grupo 7C

---
+++ clases/mi_primera_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    /*const MaterialApp(
+      home: Text('Hola Flutter',),
+    ),*/
+    //const Text('Hola Flutter',),
+    const MaterialApp(
+      home: Scaffold(
+        body: Text('Hola con Scaffold',),
+      ),
+    ),
+  );
+}
+
