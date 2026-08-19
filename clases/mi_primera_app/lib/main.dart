0db1b88c3927e71ffa2a7b6309d896962aebea46
Irving2355 <it235597@gmail.com>
2026-08-18 20:43:13 -0600

    Clase 2026-08-18 - Grupo 7B

--- clases/mi_primera_app/lib/main.dart
+++ clases/mi_primera_app/lib/main.dart
@@ -23,7 +23,28 @@ void main() {
         ),
 
         body: const Center(
-          child: Text('Centro',),
+          //child: Text('Centro',),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.phone_android,
+                size: 200,
+              ),
+              SizedBox(height: 20,),
+              Text(
+                'Texto 1',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20,),
+              Text('Texto 2',),
+              SizedBox(height: 20,),
+              Text('Texto 3',),
+            ],
+          ),
         ),
       ),
     ),
