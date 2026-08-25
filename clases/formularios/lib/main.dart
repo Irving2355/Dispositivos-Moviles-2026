147a39717d399c44c95d5f04f80d7bdf08341c6e
Irving2355 <it235597@gmail.com>
2026-08-24 20:38:24 -0600

    Clase 2026-08-24 - Grupo 7B

--- clases/formularios/lib/main.dart
+++ clases/formularios/lib/main.dart
@@ -31,10 +31,61 @@ class RegristoPage extends StatefulWidget {
 }
 
 class _RegristoPageState extends State<RegristoPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _nombreController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return /*fscaff*/  Scaffold(
+      appBar: AppBar(
+        title: const Text('Registro alumno'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+
+        child: Form(
+          key: _formKey,
+
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Nombre completo',
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(),
+                ),
+                controller: _nombreController,
+
+                validator: (value) {
+                  if(value == null || value.trim().isEmpty){
+                    return 'Dame un nombre';
+                  }
+
+                  return null;
+                },
+              ),
+
+              //fszbh
+              SizedBox(
+                height: 16,
+              ),
+
+              TextFormField(
+                controller: _correoController,
+
+                keyboardType: TextInputType.emailAddress,
+
+                decoration: InputDecoration(
+                  labelText: 'Correo',
+                  hintText: 'correo@ejemplo.com',
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
