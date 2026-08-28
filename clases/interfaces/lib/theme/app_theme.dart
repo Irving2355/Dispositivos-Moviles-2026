d5793970bfebf6a4d53d16377594895c964ade7a
Irving2355 <it235597@gmail.com>
2026-08-27 20:00:41 -0600

    Clase 2026-08-27 - Grupo 7C

---
+++ clases/interfaces/lib/theme/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData get lightTheme{
+    final colorScheme = ColorScheme.fromSeed(seedColor: Colors.indigo,);
+
+    return ThemeData(
+      colorScheme: colorScheme,
+
+      scaffoldBackgroundColor: 
+      colorScheme.surface,
+
+      textTheme: const TextTheme(
+        headlineSmall: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+
+        titleLarge: TextStyle(
+          fontWeight: FontWeight.w700,
+        )
+      ),
+    );
+  }
+}
