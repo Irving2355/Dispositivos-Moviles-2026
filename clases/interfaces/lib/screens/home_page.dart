d7b5cdc31235463e23d6499ab56a79a5ba3a690b
Irving2355 <it235597@gmail.com>
2026-08-31 20:37:49 -0600

    Clase 2026-08-31 - Grupo 7B

--- clases/interfaces/lib/screens/home_page.dart
+++ clases/interfaces/lib/screens/home_page.dart
@@ -1,5 +1,7 @@
 //fstful
 import 'package:flutter/material.dart';
+import '../data/course_data.dart';
+import '../widgets/category_chip.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({ super.key });
@@ -9,6 +11,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String _categoriaSeleccionada = 'Todos';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,11 +54,93 @@ class _HomePageState extends State<HomePage> {
                 fit: StackFit.expand,
                 children: [
                   Image.network(
-                    'https://picsum.photos/seed/campus-lerning/1200/600',
+                    'https://fotografias.lasexta.com/clipping/cmsimages02/2023/06/12/2D1E1A88-3CCB-4EF5-BFAC-65A34B6706F3/fondos-pantalla-clasicos-windows-calidad_98.jpg?crop=1300,731,x0,y0&width=1900&height=1069&optimize=high&format=webply',
                     fit: BoxFit.cover,
+                  ),
+
+                  const DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.transparent,
+                          Colors.black87,
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  const Positioned(
+                    left: 20,
+                    right: 20,
+                    bottom: 18,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+
+                      children: [
+                        Text(
+                          'Aprende algo nuevo hoy...',
+                          style: 
+                          TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
+            ),
+          ),
+
+          const SizedBox(
+            height: 22,
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Categorias',
+                style: 
+                Theme.of(context).textTheme.titleLarge,
+              ),
+
+              const Text('Explorar',),
+            ],
+          ),
+
+          const SizedBox(
+            height: 12,
+          ),
+
+          SizedBox(
+            height: 48,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: categorias.length,
+              separatorBuilder: (context, index){
+                return const SizedBox(
+                  width: 8,
+                );
+              },
+
+              itemBuilder: (context, index) {
+                final categoria = categorias[index];
+
+                return CategoryChip(
+                  label: categoria, 
+                  selected: _categoriaSeleccionada == categoria, 
+                  onTap: (){
+                    setState(() {
+                      _categoriaSeleccionada = categoria;
+                    });
+                  }
+                  );
+              },
             ),
           ),
         ],
