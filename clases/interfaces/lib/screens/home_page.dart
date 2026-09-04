76d39dbe77f0a22df86f80fc243974b4120eb4e5
Irving2355 <it235597@gmail.com>
2026-09-03 19:38:51 -0600

    Clase 2026-09-03 - Grupo 7C

--- clases/interfaces/lib/screens/home_page.dart
+++ clases/interfaces/lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../data/course_data.dart';
 import '../widgets/category_chip.dart';
+import '../widgets/course_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -30,6 +31,8 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final cursoFiltrado = _obtenerCursos();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Campus cursos',),
@@ -156,6 +159,52 @@ class _HomePageState extends State<HomePage> {
                   );
               },
             ),
+          ),
+
+          const SizedBox(
+            height: 18,
+          ),
+
+          Text('Cursos disponibles'),
+
+          const SizedBox(
+            height: 12,
+          ),
+
+          GridView.builder(
+            shrinkWrap: true,
+            physics: 
+            const NeverScrollableScrollPhysics(),
+
+            itemCount: cursoFiltrado.length,
+
+            gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+              childAspectRatio: 0.68,
+            ), 
+            
+            itemBuilder:(context, index) {
+              final curso = cursoFiltrado[index];
+
+              return CourseCard(
+                titulo: curso['titulo']!, 
+                categoria: curso['categoria']!, 
+                nivel: curso['nivel']!, 
+                duracion: curso['duracion']!, 
+                imagen: curso['imagen']!, 
+                onPressed: (){
+                  ScaffoldMessenger
+                  .of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Seleccionado ${curso['titulo']}'),
+                    ),
+                  );
+                }
+              );
+            },
           ),
         ],
       ),
