e17b0289d152a7032bd54c140d2e6bd03c4969cb
Irving2355 <it235597@gmail.com>
2026-09-07 12:37:10 -0600

    Clase 2026-09-07 - Grupo 7A

--- clases/interfaces/lib/screens/home_page.dart
+++ clases/interfaces/lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../data/course_data.dart';
 import '../widgets/category_chip.dart';
+import '../widgets/course_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -30,6 +31,9 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final cursoFiltrado = _obtenerCursos();
+
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Campus cursos'),
@@ -151,6 +155,45 @@ class _HomePageState extends State<HomePage> {
                 );
               },
             ),
+          ),
+
+          SizedBox(
+            height: 20,
+          ),
+
+          GridView.builder(
+            shrinkWrap: true,
+            physics: 
+            const NeverScrollableScrollPhysics(),
+
+            itemCount: cursoFiltrado.length,
+            gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+              childAspectRatio: 0.68,
+            ), 
+            
+            itemBuilder:(context, index) {
+              final curso = cursoFiltrado[index];
+
+              return CourseCard(
+                titulo: curso['titulo']!, 
+                categoria: curso['categoria']!, 
+                nivel: curso['nivel']!, 
+                duracion: curso['duracion']!, 
+                imagen: curso['imagen']!, 
+                onPressed: (){
+                  ScaffoldMessenger.of(context)
+                  .showSnackBar(
+                    SnackBar(
+                      content: Text('Seleccionado')
+                      ),
+                  );
+                }
+              );
+            },
           ),
         ],
       ),
