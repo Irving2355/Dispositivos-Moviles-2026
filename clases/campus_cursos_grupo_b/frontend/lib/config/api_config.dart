0c56a000a0b030426d29cb1d663b2a2fd59d3f71
Irving2355 <it235597@gmail.com>
2026-09-23 16:42:35 -0600

    Clase 2026-09-23 - Grupo 7B

---
+++ clases/campus_cursos_grupo_b/frontend/lib/config/api_config.dart
@@ -0,0 +1,9 @@
+class ApiConfig {
+  const ApiConfig._();
+
+  static const String baseUrl = 
+    String.fromEnvironment(
+      'API_URL',
+      defaultValue: 'http://localhost:3000/api',
+    );
+}
